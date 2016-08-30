840c7c852cd4918c2685e821cf343b2e4bf67e07
davemackintosh <me@davemackintosh.co.uk>
2016-08-30 18:49:56 +0100

    testing.

--- tests/adapter.js
+++ tests/adapter.js
@@ -1,24 +1,36 @@
 "use strict"
 
+const {symbols} = require("../lib/recommended")
 const Adapter_Interface = require("../lib/validation/adapter-interface")
+const {MongoClient} = require("mongodb")
 
 class MongoAdapter extends Adapter_Interface {
+  get [symbols.IS_SCHEMA_ONLY]() {
+    return true
+  }
+
   constructor() {
     super()
-
-    console.log("THIS", this);
   }
 
   connect(config) {
-
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(config.url, config.options, (err, connection) => {
+        if (err) reject(err)
+        else {
+          this.connection = connection
+          resolve()
+        }
+      })
+    })
   }
 
   disconnect() {
-
+    return this.connection.close()
   }
 
   describe() {
-
+    
   }
 
   update(criteria, values, options) {
